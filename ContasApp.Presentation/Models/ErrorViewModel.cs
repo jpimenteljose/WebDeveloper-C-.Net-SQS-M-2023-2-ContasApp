3438cd5ee4dd915c5d146a6e67cab5e0bf3903d4
jpimenteljose <jpimentel.jose@hotmail.com>
2023-08-28 16:17:39 -0300

    feat: Criação das páginas de Login, Register e ForgotPassword

--- ContasApp.Presentation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ContasApp.Presentation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
